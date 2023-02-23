9b73878d6b7115d3e280a147c446481b309343d2
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-02-23 20:32:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3 
 # coding: utf-8
 
+"""
+This is setup script which will set the environment for client.py program
+"""
+
+import sys
+from os import system as cmd
+
 class Server:
     hostname = "X-Hall"
-    ip = '10.184.26.49'
-    port = 1234
+    ip = input("[ ] Enter Host IP: ")
+    port = int(input("[ ] Enter port to connect: "))
 
