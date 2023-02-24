197629e3d8819c0d97a514ff51371b2f91ab3883
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-02-24 17:20:55 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,16 +2,58 @@
 # coding: utf-8
 
 import socket
-import _thread
+import threading
 import sys
 
+class Client:
+	def __init__(self, client_sock, hostname):
+		self.sock = client_sock
+		self.name = ""
+		self.HOSTNAME = hostname
+
+	def send_banner(self):
+		try:
+			banner = f"[ ---------- WELCOME TO THE '{self.HOSTNAME}' CHATROOM ---------- ]"
+			self.sock.send(banner.encode('utf-8'))
+		except Exception as e:
+			print("[error]",str(e))
+			self.sock.close()
+
+	def get_name(self):
+		try:
+			self.name = self.sock.recv(1024).decode('utf-8')
+		except Exception as e:
+			print("[error]",str(e))
+			self.sock.close()
+	
+	def send_messages(self):
+		while True:
+			try:
+				data = input(f"<{self.HOSTNAME} *>: ")
+				data = f"<{self.HOSTNAME}>: "+data
+				self.sock.send(data.encode('utf-8'))
+			except KeyboardInterrupt:
+				self.sock.close()
+				break
+	
+	def recv_messages(self):
+		while True:
+			try:
+				data = self.sock.recv(1024).decode('utf-8')
+				if not data: continue
+				# prettify data
+				print(data)
+			except ConnectionResetError:
+				print(f"[<{self.name}> left!]")
+				self.sock.close()
+				break 
+		
 class Server:
 	def __init__(self, ip, port, name, max_connections=5):
-		self.HOST = ip 
+		self.HOSTIP = ip 
 		self.PORT = port 
 		self.HOSTNAME = name 
-		self.client_name = dict()
-		self.MAX_CONNECTIONS = max_connections
+		self.max_connections = max_connections
 
 		try:
 			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,57 +61,48 @@ class Server:
 			print("[!] Failed to create a socket")
 			print("[error]",str(e))
 			sys.exit()
-	
-	def send_message(self, client):
-		while True:
-			try:
-				data = input(f"<{self.HOSTNAME} *>: ")
-				data = f"<{self.HOSTNAME}>: "+data
-				client.send(data.encode('utf-8'))
-			except KeyboardInterrupt:
-				break
-    
-	def recv_message(self, client):
-		while True:
-			try:
-				data = client.recv(1024).decode('utf-8')
-				if not data: continue
-				# prettify data
-				print(data)
-			except ConnectionResetError:
-				print(f"[<{self.client_name[client]}> left!]")
-				break
 
 	def chat(self, client):
 		try:
-			client.send("[ ---------- WELCOME TO THE 'X-HALL' CHATROOM ---------- ]".encode('utf-8'))
-			_thread.start_new_thread(self.send_message, (client,))
-			_thread.start_new_thread(self.recv_message, (client,))
+			client.send_banner()
+			threading.Thread(target=client.send_messages, daemon=True).start()
+			threading.Thread(target=client.recv_messages, daemon=True).start()
+			threading.Event().wait()
 		except KeyboardInterrupt:
 			self.sock.shutdown(2)
 			print("[shutdown!]")
+			sys.exit()
 		except ConnectionResetError:
-			client.close()
-			print(f"[<{self.client_name[client]}> left!]")
-		
+			print(f"[<{self.name}> left!]")
+			client.sock.close()
+		except Exception as e:
+			print("[error]",str(e))
+			self.sock.shutdown(2)
+			print("[shutdown!]")
+			sys.exit()
+
 	def start(self):
 		try:
-			self.sock.bind((self.HOST, self.PORT))
+			self.sock.bind((self.HOSTIP, self.PORT))
 			# maximum connections 5
-			self.sock.listen(self.MAX_CONNECTIONS)
-			print(f"[+] Waiting for Connections at {self.HOST}:{self.PORT}")
+			self.sock.listen(self.max_connections)
+			print(f"[+] Waiting for Connections at {self.HOSTIP}:{self.PORT}")
 			
 			while True:
-				client, addr = self.sock.accept()
-				self.client_name[client] = client.recv(1024).decode('utf-8')
-				print(f"[+] <{self.client_name[client]}> joined from {addr[0]}:{addr[1]}"	)
-				_thread.start_new_thread(self.chat, (client,))
+				client_sock, addr = self.sock.accept()
+				client = Client(client_sock, self.HOSTNAME)
+				client.name = client.get_name()
+				print(f"[+] <{client.name}> joined from {addr[0]}:{addr[1]}")
+				threading.Thread(target=self.chat, daemon=True, args=(client,)).start()
+				# threading.Event().wait()
 		except socket.error as e:
 			print("[!] Failed to start server")
 			print("[error]", str(e))
 			sys.exit()
 
 if __name__ == '__main__':
-	host = "10.184.26.49"
+	host = input("Enter host IP: ")
+	# port = int(input("Enter Port: "))
+	# host = "10.7.10.71"
 	port = 1234
 	Server(host, port, 'X-Hall').start()
