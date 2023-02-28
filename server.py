e9207a31bdec4fc63d4e74699124c8c80f64443a
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-02-28 04:51:27 +0000

    Update server.py

--- server.py
+++ server.py
@@ -5,104 +5,127 @@ import socket
 import threading
 import sys
 
+# for unique usernames
+from time import time as now
+
+# INHERITENCE STUFF TO BE DONE
 class Client:
-	def __init__(self, client_sock, hostname):
-		self.sock = client_sock
-		self.name = ""
-		self.HOSTNAME = hostname
+    def __init__(self, client_sock, hostname):
+        self.sock = client_sock
+        self.name = ""
+        self.HOSTNAME = hostname
+    
+    def exists(self, name):
+        # check the username if already active on the server
+        return False
 
-	def send_banner(self):
-		try:
-			banner = f"[ ---------- WELCOME TO THE '{self.HOSTNAME}' CHATROOM ---------- ]"
-			self.sock.send(banner.encode('utf-8'))
-		except Exception as e:
-			print("[error]",str(e))
-			self.sock.close()
+    def send_banner(self):
+        banner = f"[ ---------- WELCOME TO THE '{self.HOSTNAME}' CHATROOM ---------- ]"
+        self.send_message(banner)
 
-	def get_name(self):
-		try:
-			self.name = self.sock.recv(1024).decode('utf-8')
-		except Exception as e:
-			print("[error]",str(e))
-			self.sock.close()
-	
-	def send_messages(self):
-		while True:
-			try:
-				data = input(f"<{self.HOSTNAME} *>: ")
-				data = f"<{self.HOSTNAME}>: "+data
-				self.sock.send(data.encode('utf-8'))
-			except KeyboardInterrupt:
-				self.sock.close()
-				break
-	
-	def recv_messages(self):
-		while True:
-			try:
-				data = self.sock.recv(1024).decode('utf-8')
-				if not data: continue
-				# prettify data
-				print(data)
-			except ConnectionResetError:
-				print(f"[<{self.name}> left!]")
-				self.sock.close()
-				break 
-		
+    def set_username(self):
+        self.send_message(f"[username? @{self.HOSTNAME}] ")
+        response = self.recv_message().strip()
+        if len(response)>0 and self.exists(response)==False:
+            self.name = response
+        else:
+            self.name = f"user{now}"
+            self.send_message(f"[{self.HOSTNAME}] Username Invalid! You are {self.name}")
+
+    def send_message(self, message):
+        try:
+            message = f"<{self.name}>@{self.HOSTNAME}: "+message
+            self.sock.send(message.encode(ENCODING))
+        except ConnectionError:
+            print(f"[<{self.name}> DISCONNECTED!]")
+            self.sock.close()
+
+    def recv_message(self):
+        try:
+            data = self.sock.recv(BUFFERSIZE).decode(ENCODING)
+            return data
+        except ConnectionError:
+            print(f"[<{self.name}> DISCONNECTED!]")
+            self.sock.close()
+
+
 class Server:
-	def __init__(self, ip, port, name, max_connections=5):
-		self.HOSTIP = ip 
-		self.PORT = port 
-		self.HOSTNAME = name 
-		self.max_connections = max_connections
+    def __init__(self, ip, port, hostname):
+        self.HOSTIP = ip 
+        self.PORT = port 
+        self.HOSTNAME = hostname 
+        self.clients = []
 
-		try:
-			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		except socket.error as e:
-			print("[!] Failed to create a socket")
-			print("[error]",str(e))
-			sys.exit()
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as e:
+            print("[!] Failed to create a socket")
+            print("[error]",str(e))
+            sys.exit()
 
-	def chat(self, client):
-		try:
-			client.send_banner()
-			threading.Thread(target=client.send_messages, daemon=True).start()
-			threading.Thread(target=client.recv_messages, daemon=True).start()
-			threading.Event().wait()
-		except KeyboardInterrupt:
-			self.sock.shutdown(2)
-			print("[shutdown!]")
-			sys.exit()
-		except ConnectionResetError:
-			print(f"[<{self.name}> left!]")
-			client.sock.close()
-		except Exception as e:
-			print("[error]",str(e))
-			self.sock.shutdown(2)
-			print("[shutdown!]")
-			sys.exit()
+    def chat(self, client):
+        try:
+            client.send_banner()
+            client.get_name()
+            self.broadcast(f"[+] <{client.name}> joined the chat room.")
+            self.clients.append(client)
+            threading.Thread(target=client.send_messages, daemon=True).start()
+            threading.Thread(target=client.recv_messages, daemon=True).start()
+            threading.Event().wait()
+        except KeyboardInterrupt:
+            self.sock.shutdown(2)
+            print("[shutdown!]")
+            sys.exit()
+        except ConnectionResetError:
+            self.clients.remove(client)
+            self.broadcast(f"[<{client.name}> left!]")
+            client.sock.close()
+        except Exception as e:
+            print("[error]",str(e))
+            self.sock.shutdown(2)
+            print("[shutdown!]")
+            sys.exit()
 
-	def start(self):
-		try:
-			self.sock.bind((self.HOSTIP, self.PORT))
-			# maximum connections 5
-			self.sock.listen(self.max_connections)
-			print(f"[+] Waiting for Connections at {self.HOSTIP}:{self.PORT}")
-			
-			while True:
-				client_sock, addr = self.sock.accept()
-				client = Client(client_sock, self.HOSTNAME)
-				client.name = client.get_name()
-				print(f"[+] <{client.name}> joined from {addr[0]}:{addr[1]}")
-				threading.Thread(target=self.chat, daemon=True, args=(client,)).start()
-				# threading.Event().wait()
-		except socket.error as e:
-			print("[!] Failed to start server")
-			print("[error]", str(e))
-			sys.exit()
+    def broadcast(self, message):
+        for client in self.clients:
+            try:
+                client.send_message(message)
+            except:
+                self.clients.remove(client)
+
+    def start(self):
+        try:
+            self.sock.bind((self.HOSTIP, self.PORT))
+            # maximum connections 5
+            self.sock.listen(MAX_CONNECTIONS)
+            print(f"[+] Waiting for Connections at {self.HOSTIP}:{self.PORT}")
+            
+            while True:
+                client_sock, addr = self.sock.accept()
+                client = Client(client_sock, self.HOSTNAME)
+                print(f"[+] New client connected from {addr[0]}:{addr[1]}")
+                threading.Thread(target=self.chat, daemon=True, args=(client,)).start()
+                # threading.Event().wait()
+        except socket.error as e:
+            print("[!] Failed to start server")
+            print("[error]", str(e))
+            sys.exit()
+
+
+
 
 if __name__ == '__main__':
-	host = input("Enter host IP: ")
+
+    # constants
+    BUFFERSIZE = 1024
+    ENCODING = 'utf-8'
+    MAX_CONNECTIONS = 5
+    HOSTNAME = "X-HALL"
+    
+    
+    host = input("Enter host IP: ")
 	# port = int(input("Enter Port: "))
 	# host = "10.7.10.71"
-	port = 1234
-	Server(host, port, 'X-Hall').start()
+    port = 1234
+    Server(host, port, 'X-Hall').start()
+	
