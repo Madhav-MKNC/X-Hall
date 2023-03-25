da898ab6cb4cfa9fe7ff7a0fca686a7eef38f3b1
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-03-25 15:15:34 +0530

    Update setup.py

--- client/setup.py
+++ client/setup.py
@@ -11,16 +11,20 @@ from os import system as cmd
 
 def filterinfo(ip,port):
     try:
+        port = int(port)
+
+# other host names also valid so will update this using sockets
         if ip=='localhost':
             if 0<port<65535:
                 return ip, port
+# use IPv4 filtering here
         elif all(0<i<256 for i in list(map(int,ip.split('.')))) and 0<port<2**16: 
             return ip, port
     except:
         pass
     
     print(ip,port)
-    print("[!] Invalid Host IP or Port input, using default [localhost:1234]")
+    print("[!] using default [localhost:1234]")
     return 'localhost',1234
     # default host => 'localhost'
     # default port => 1234
@@ -29,18 +33,16 @@ class Host:
     def __init__(self) -> None:
         try:
             with open('.hostinfo','r') as file:
-                info = file.read()
-            self.ip, self.port = filterinfo(info)
+                info = file.read().split(":")
+            self.ip, self.port = filterinfo(map(str, info))
         except Exception as e:
             print("[error]",str(e))
-            self.ip, self.port = filterinfo(info)
-            print("[+] using default [localhost:1234]")
-            sys.exit()
+            self.ip, self.port = filterinfo(0,0)
 
 
 if __name__ == "__main__":
     ip = input("[=] Enter Host IP: ")
-    port = int(input("[=] Enter port to connect: "))
+    port = input("[=] Enter port to connect: ")
 
     ip, port = filterinfo(ip,port)
     info = f"{ip}:{port}"
@@ -56,4 +58,3 @@ if __name__ == "__main__":
             cmd("pip install -r requirements.txt")
     except Exception as e:
         print("[!]",str(e))
-    
